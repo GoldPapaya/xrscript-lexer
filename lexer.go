2ac497abad42e1f48a26a4dcd865a5d51a7e2254
GoldPapaya <lucasdcrawford32@gmail.com>
2025-07-28 15:05:30 -0400

    token setup and printing

--- lexer.go
+++ lexer.go
@@ -1,9 +1,40 @@
-package main
+package main // temp
 
 import (
-	"fmt"
+
 )
 
-func main() {
-	fmt.Println("hello world")
+type TokenType int
+
+const (
+	EOF TokenType = iota // End of file (EOF) token, start value 0
+	IDENTIFIER 	         // variable ids, value 1 etc.
+	NUMBER			     // floats and ints
+	ADD				     // +
+	SUB				     // -
+	MUL				     // *
+	DIV				     // /
+	MOD				     // %
+	ASSIGN			     // =
+	DOT				     // . (Currently just for use in floats)
+	SEMICOLON		     // ;
+)
+
+var tokens = []string{
+	EOF: "EOF",
+	IDENTIFIER: "IDENTIFIER",
+	NUMBER: "NUMBER",
+	ADD: "ADD",
+	SUB: "SUB",
+	MUL: "MUL",
+	DIV: "DIV",
+	MOD: "MOD",
+	ASSIGN: "ASSIGN",
+	DOT: "DOT",
+	SEMICOLON: "SEMICOLON",
+}
+
+// Print token method
+func (t TokenType) String() string {
+	return tokens[t]
 }
