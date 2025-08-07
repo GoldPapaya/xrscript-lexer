6ccefa63f2a4ec1dbdcd5a3daffd7f276f323dd7
GoldPapaya <lucasdcrawford32@gmail.com>
2025-08-06 21:30:10 -0400

    moved objects and sup fxns to helper.go

--- lexer.go
+++ lexer.go
@@ -1,74 +1,10 @@
 package main // temp
 
 import (
-	"bufio"
 	"fmt"
 	"io"
 	"unicode"
 )
-
-type TokenType int
-
-const (
-	EOF TokenType = iota 	 // End of file (EOF) token, start value 0
-	IDENTIFIER 	         // variable ids, value 1 etc.
-	NUMBER			     // floats and ints
-	ADD				     // +
-	SUB				     // -
-	MUL				     // *
-	DIV				     // /
-	MOD				     // %
-	ASSIGN			     // =
-	DOT				     // . (Currently just for use in floats)
-	SEMICOLON		     // ;
-)
-
-var tokenLiterals = []string{
-	EOF: "EOF",
-	IDENTIFIER: "IDENTIFIER",
-	NUMBER: "NUMBER",
-	ADD: "ADD",
-	SUB: "SUB",
-	MUL: "MUL",
-	DIV: "DIV",
-	MOD: "MOD",
-	ASSIGN: "ASSIGN",
-	DOT: "DOT",
-	SEMICOLON: "SEMICOLON",
-}
-
-// Print token method, mostly for testing
-func (t TokenType) String() string {
-	return tokenLiterals[t]
-}
-
-type Position struct {
-	row int
-	col int
-}
-
-type Token struct {
-	pos Position
-	typ TokenType
-	literal string
-}
-
-type Lexer struct {
-	pos Position
-	reader bufio.Reader
-}
-
-func NewLexer(reader io.Reader) *Lexer {
-	return &Lexer{
-		pos: Position{row: 1, col: 0},
-		reader: *bufio.NewReader(reader),
-	}
-}
-
-func (l *Lexer) newline() {
-	l.pos.row += 1
-	l.pos.col = 0
-}
 
 func (l *Lexer) Lex() (Token) {
 	for {
